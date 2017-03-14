7e20ba0fa7102ec888edb3178f31fc164d512def
bfleury2017 <bfleury@outlook.com>
2017-03-13 23:04:03 -0400

    Initial commit

---
+++ app/src/main/java/com/everykindred/transitiondemo/TransitionDemoActivity.java
@@ -0,0 +1,58 @@
+package com.everykindred.transitiondemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.BounceInterpolator;
+import android.widget.RelativeLayout;
+
+public class TransitionDemoActivity extends AppCompatActivity {
+
+    private ViewGroup myLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transition_demo);
+
+        myLayout = (ViewGroup) findViewById(R.id.myLayout);
+
+        myLayout.setOnTouchListener(new RelativeLayout.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                handleTouch();
+                return true;
+            }
+        });
+    }
+
+    public void handleTouch() {
+        View view = findViewById(R.id.myButton1);
+
+        Transition changeBounds = new ChangeBounds();
+        changeBounds.setDuration(3000);
+        changeBounds.setInterpolator(new BounceInterpolator());
+
+        TransitionManager.beginDelayedTransition(myLayout, changeBounds);
+        
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+
+        view.setLayoutParams(params);
+
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.width = 500;
+        layoutParams.height = 350;
+        view.setLayoutParams(layoutParams);
+
+    }
+
+
+}
